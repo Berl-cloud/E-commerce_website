502218402ed940f9840ea040fb27832e35fd9de0
Berlinda Anaman <berlinda.anaman@amalitech.org>
2021-06-14 20:24:06 +0000

    Adding items to cart ang getting total cost

--- script.js
+++ script.js
@@ -38,134 +38,154 @@ function color1() {
 
   /************************************INCREASING AND REDUCING ITEM QUANTITY************************************/
 
-  let input = document.getElementById("qtyNumber");
+//   let input = document.getElementById("qtyNumber");
 
-  function minus(){
-    let num = +input.value; //+ for convert from string to number
-      if (num > 1) {
-          num--;
-      } else {
-          num;
-      }
-    input.value = num;
+//   function minus(){
+//     let num = +input.value; //+ for convert from string to number
+//       if (num > 1) {
+//           num--;
+//       } else {
+//           num;
+//       }
+//     input.value = num;
     
-  }
+//   }
   
-  function plus(){
-    let num = +input.value; //+ for convert from string to number
-    if (num < 100) {
-        num++;
-    } else {
-        num;
-    }
-    input.value = num;
-  }
+//   function plus(){
+//     let num = +input.value; //+ for convert from string to number
+//     if (num < 100) {
+//         num++;
+//     } else {
+//         num;
+//     }
+//     input.value = num;
+//   }
 
   /************************************ITEM QUANTITY TO SHOW IN CART************************************/
+document.addEventListener('DOMContentLoaded', ()=>{
+    init();
+    
+})
 
-let carts = document.querySelectorAll('#addCart');
+let cartItems = localStorage.setItem('productsInCart', '');
+localStorage.setItem('cartNumbers','');
+let inputqty = document.getElementById('qtyNumber');
+localStorage.setItem('totalCost', '');
+let productNumbers;
+let total;
 
-let products = [
-    {
-        name: 'Couple laptop backpack USB charging 3-piece',
-        tag: 'laptopbag',
-        price: 200,
-        inCart: 0
-    }    
-];
 
-for ( let i = 0; i < carts.length; i++) {
-    carts[i].addEventListener("click", () => {
-    cartNumbers(products[i]);
-    totalCost(products[i]);
+function init(){
+    
+    // qtyPlus();
 
+    let carts = document.querySelector('#btn_addCart');
+
+    let products = 
+        {
+            name: 'Couple laptop backpack USB charging 3-piece',
+            tag: 'laptopbag',
+            price: 200,
+            inCart: 0
+        };
+    
+        carts.addEventListener("click", () => {
+        //    slide();
+        document.getElementById('cart-confirmation').style.display='block';
+        cartNumbers(products);
+        totalCost(products);
+        
+        })
+
+        // function slide() {
+        //     let slide_in = document.getElementById('cart-confirmation'); 
+        //     // slide_in.attr('id')
+        //     console.log(slide_in.id)
+        //     slide_in.id = 'cart-confirmation-slide'
+        // }
+    
+        // function qtyPlus() {
+    //     inputqty.addEventListener('change', () => {
+    //     })
+
+    //     // inputqty.addEventListener('keydown', (b) => {
+    //     //     inputqty.value=b.Number(b.keyCode)
+    //     // })
+    // }
+//    productNumbers = .value + productNumbers
+    //ADDING ITEMS TO CART
+    function cartNumbers (product) {
+
+        if (inputqty.value) {
+            productNumbers = Number(inputqty.value) + Number(localStorage.getItem('cartNumbers'));
+        localStorage.setItem('cartNumbers', Number(productNumbers) );
+        } else
+        console.log(productNumbers)
+        document.querySelector('.cart span').textContent = productNumbers;
+        document.querySelector('.cart-span').textContent = productNumbers; 
+    }
+
+    function keepCartNumbers() {
+        productNumbers = localStorage.getItem('cartNumbers');
+    
+        if(productNumbers) {
+            document.querySelector('.cart span').textContent = productNumbers;
+        }
+    }
+    
+    function setItems(product) {
+       
+        cartItems = JSON.parse(cartItems);
+    
+        if (cartItems != null) {
+             if(cartItems[product.tag] == undefined) {
+                 cartItems = { ...cartItems, [product.tag]: product }
+             
+            }
+            cartItems[product.tag].inCart += 1;
+        } else {
+            product.inCart = 1;
+            cartItems = {
+                [product.tag]: product
+            }
+        }
+    
+        localStorage.setItem('productsInCart', JSON.stringify (cartItems));
+    }
+    
+    function totalCost(product) {
+        total =(productNumbers * products.price);
+    
+        if(total != null) {
+            total = parseFloat(total);
+            localStorage.setItem('totalCost', total)
+            document.querySelector('.cart-confirmation-subtotal p').textContent = total;
+            document.querySelector('.cart-confirmation-subtotal_p').textContent = total;
+            
+        } else {
+            localStorage.setItem('totalCost', product.price);
+            document.querySelector('.cart-confirmation-subtotal p').textContent = product.price;
+        }
+        document.querySelector('.order-summary-body subtotal').innerText = total;
+         
+    }
+    
+    
+    
+    /************************ JAVASCRIPT FOR CART PAGE**********************/ 
+    
+    let close = document.getElementById('cart-confirmationClosebtn');
+        close.addEventListener('click', function (event) {
+        let closeClicked = event.target;
+        closeClicked.parentElement.parentElement.remove();
     })
     
-}
-
-
-function keepCartNumbers() {
-    let productNumbers = localStorage.getItem('cartNumbers');
-
-    if(productNumbers) {
-        document.querySelector('.cart span').textContent = productNumbers;
-    }
-}
-
-function cartNumbers (product) {
-    let productNumbers = localStorage.getItem('cartNumbers');
-    productNumbers = parseInt(productNumbers);
+    // let button = document.getElementById('deleteCartItem');
+    //     button.addEventListener('click', function (event) {
+    //     let buttonClicked = event.target;
+    //     buttonClicked.parentElement.parentElement.parentElement.remove();
+    // })
     
-   if (productNumbers) {
-    localStorage.setItem('cartNumbers', productNumbers + 1 );
-    document.querySelector('.cart span').textContent = productNumbers + 1;
-    document.querySelector('.cart-confirmation').classList.add('slide') = ;
-    // document.querySelector('product-qtysubtotal span').innerHTML = productNumbers + 1;
-   } else{
-    localStorage.setItem('cartNumbers', 1);
-    document.querySelector('.cart span').textContent = 1;
-   } 
-   
-   setItems(product);
-}
-
-function setItems(product) {
-    let cartItems = localStorage.getItem('productsInCart');
-    cartItems = JSON.parse(cartItems);
-
-    if (cartItems != null) {
-         if(cartItems[product.tag] == undefined) {
-             cartItems = { ...cartItems, [product.tag]: product }
-         
-        }
-        cartItems[product.tag].inCart += 1;
-    } else {
-        product.inCart = 1;
-        cartItems = {
-            [product.tag]: product
-        }
-    }
-
-    localStorage.setItem('productsInCart', JSON.stringify (cartItems));
-}
-
-function totalCost(product) {
     
-    let total = localStorage.getItem('totalCost');
-    total =parseFloat(cartItems[product.tag].inCart * product.price);
 
-    if(total != null) {
-        total = parseFloat(total);
-        localStorage.setItem('totalCost', total)
-        document.querySelector('.cart-confirmation-subtotal p').textContent = total;
-        document.querySelector('.productTotal span').textContent = total;
-        // document.querySelector('.cart-confirmation').style.display = 'block';
-    } else {
-        localStorage.setItem('totalCost', product.price);
-        document.querySelector('.cart-confirmation-subtotal p').textContent = product.price;
-    }
-    // document.getElementById('productTotal').innerHTML = total
-    
 }
-
-
-
-/************************ JAVASCRIPT FOR CART PAGE**********************/
-
-
-
-keepCartNumbers();
-
-
-let close = document.getElementById('cart-confirmationClosebtn');
-    close.addEventListener('click', function (event) {
-    let closeClicked = event.target;
-    closeClicked.parentElement.parentElement.remove();
-})
-
-let button = document.getElementById('deleteCartItem');
-    button.addEventListener('click', function (event) {
-    let buttonClicked = event.target;
-    buttonClicked.parentElement.parentElement.parentElement.remove();
-})
-
